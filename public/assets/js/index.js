8769868f01d4cfeea9349095b420c9267cd1172a
James Broski <james.broski.5@gmail.com>
2021-08-29 22:03:59 -0600

    working as intended

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -53,7 +53,7 @@ const deleteNote = (id) =>
 const renderActiveNote = () => {
   hide(saveNoteBtn);
 
-  if (activeNote.id) {
+  if (activeNote.note_id) {
     noteTitle.setAttribute("readonly", true);
     noteText.setAttribute("readonly", true);
     noteTitle.value = activeNote.title;
@@ -178,8 +178,6 @@ if (window.location.pathname === "/notes") {
   newNoteBtn.addEventListener("click", handleNewNoteView);
   noteTitle.addEventListener("keyup", handleRenderSaveBtn);
   noteText.addEventListener("keyup", handleRenderSaveBtn);
-  noteList.addEventListener("click", ".delete-note", handleNoteDelete);
-  noteList.addEventListener("click", ".list-group-item", handleNoteView);
 }
 
 getAndRenderNotes();
